04271c8dd20e4a46bf2eb2876adb837f02954fcb
Moshe <moshe@example.com>
2026-01-21 17:15:31 +0200

    update correct  name function

--- server-b/routes.py
+++ server-b/routes.py
@@ -4,11 +4,11 @@ from shared.schemas import LstLocation
 router = APIRouter()
     
 @router.post("/clean")
-def clean_send(data: list[models.LocationData]):
-    data_as_dicts = [item.model_dump() for item in data]
-    df = utils.convert_to_df(data_as_dicts)
-    complete_df = utils.add_columns(df)
-    json_df = utils.df_to_json(complete_df)
-    response = utils.send_server_c(json_df)
+def clean_send(data: LstLocation):
+    data_as_dicts = [item.model_dump() for item in data.location_info]
+    df = convert_to_df(data_as_dicts)
+    complete_df = add_columns(df)
+    json_df = df_to_json(complete_df)
+    response = send_server_c(json_df)
     return response
 
