e54c448fd72ce7f13807bce6d2da0f7c67e82212
Moshe <moshe@example.com>
2026-01-21 15:30:19 +0200

    function for creating schema

--- server-c/db/mysql_db.py
+++ server-c/db/mysql_db.py
@@ -25,5 +25,36 @@ class MySQLManager():
             yield connection
         finally:
             connection.close()
+    
+    def init_schema(self):
+        
+        table_name = os.getenv('TABLE_NAME','records_table')
+        
+        conn : MySQLConnectionAbstract = self.pool.get_connection()
+        
+        try:
+            cursor : MySQLCursorAbstract = conn.cursor()
+            query: str = f"""
+                CREATE TABLE IF NOT EXISTS {table_name} (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    timestamp DATETIME,
+                    location_name VARCHAR,
+                    country VARCHAR,
+                    latitude FLOAT,
+                    longitude FLOAT,
+                    temoerature FLOAT,
+                    wind_speed FLOAT,
+                    humidity INT,
+                    temperature_category VARCHAR(100),
+                    wind_category VARCHAR(100)
+                )
+            """
+            cursor.execute(query)
+            conn.commit()
+            cursor.close()
+            
+        finally:
+            conn.close()
+                
 
 db_manager = MySQLManager(host=DATABASE_HOST,user=DATABASE_USER,password=DATABASE_PASSWORD,database_name=DATABASE_NAME)
