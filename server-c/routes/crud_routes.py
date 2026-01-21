313e73130ef57a5684646317a35857de580163fb
Moshe <moshe@example.com>
2026-01-22 00:37:58 +0200

    finish endpoint

--- server-c/routes/crud_routes.py
+++ server-c/routes/crud_routes.py
@@ -10,10 +10,35 @@ DATABASE_NAME = os.getenv("DATABASE_NAME","sql_db")
 router = APIRouter()
 
 @router.post('/records')
-def save_records(database : MySQLConnectionAbstract = Depends(db_manager.get_db)):
-    cursor = database.cursor(dictionary=True)
-    columns = ['timestamp','location_name','country','latitude','longitude','temoerature','wind_speed','humidity','temperature_category','wind_category']
-    flags = '%s' * len(columns)
-    cursor.execute(
-        f"""INSERT INTO {TABLE_NAME} ({','.join(columns)}) VALUES ({flags}),
-        """)
+def save_records(payload: LstLocationForDB, database : MySQLConnectionAbstract = Depends(db_manager.get_db)):
+    data = payload.locations_info
+    fields = list(LocationForDB.model_fields.keys())
+    values = [
+        tuple(item.model_dump().values()) 
+        for item in data
+    ]
+    try:
+        cursor = database.cursor(dictionary=True)
+        columns = ','.join(fields)
+        flags = ','.join(['%s'] * len(fields))
+        cursor.execute(f"USE {DATABASE_NAME}")
+
+        query = f"INSERT INTO {TABLE_NAME} ({columns}) VALUES ({flags})"
+        cursor.executemany(query,values)
+        rows_inserted = cursor.rowcount
+        database.commit()
+            
+        return {
+            "status": "success",
+            "message": f"Successfully inserted {rows_inserted} records",
+            "count": rows_inserted
+        }
+    except Exception as e:
+        
+        database.rollback()
+        return {
+            "status": "error",
+            "message": f"Failed to save records: {str(e)}"
+        }
+    finally:
+        cursor.close()
