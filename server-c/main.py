445460f0534dce0ff75e4d690334a1037abc0088
Moshe <moshe@example.com>
2026-01-22 00:28:55 +0200

    fix: change port from 8000 to 8002 for local runing

--- server-c/main.py
+++ server-c/main.py
@@ -23,4 +23,4 @@ app.include_router(crud_router)
 app.include_router(query_router)
 
 if __name__=='__main__':
-    uvicorn.run(app,host='0.0.0.0',port=8000)
+    uvicorn.run(app,host='0.0.0.0',port=8002)
