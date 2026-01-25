29464be25141a1ecda476e0ecb3436765ebf6a01
Moshe <moshe@example.com>
2026-01-25 09:44:36 +0200

    clean

--- server-b/app/utils.py
+++ server-b/app/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import requests
 import os
-from shared.schemas import LstLocationForDB,LocationForDB
+from shared.schemas import LstLocationForDB
 
 SERVER_C_URL = os.getenv("SERVER_C_URL", "http://localhost:8002/records")
 
